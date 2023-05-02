78d78165488c30e4d6da0036a8b55165a8807486
JustOneSummer <myyuyukosama@gmail.com>
2023-05-02 21:54:17 +0800

    类型调整2

--- src/main/java/com/shinoaki/cqhttp/sdk/bot/model/protocol/RequestData.java
+++ src/main/java/com/shinoaki/cqhttp/sdk/bot/model/protocol/RequestData.java
@@ -4,5 +4,5 @@ package com.shinoaki.cqhttp.sdk.bot.model.protocol;
  * @author Xun
  * @date 2023/5/2 21:24 星期二
  */
-public record RequestData() {
+public record RequestData<T>(Action action, T params, String echo) {
 }
